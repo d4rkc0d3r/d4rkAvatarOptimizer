c837a716dda7339fc98ed2cb928791261be9df9d
d4rkc0d3r <1735522+d4rkc0d3r@users.noreply.github.com>
2021-06-09 15:41:15 +0200

    Initial commit

---
+++ d4rkAvatarOptimizer.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class d4rkAvatarOptimizer : MonoBehaviour
+{
+    public bool MergeBackFaceCullingWithCullingOff = true;
+}
---
+++ d4rkAvatarOptimizerEditor.cs
@@ -0,0 +1,80 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(d4rkAvatarOptimizer))]
+public class d4rkAvatarOptimizerEditor : Editor
+{
+    static d4rkAvatarOptimizer t;
+
+    private static bool CanCombineWith(List<SkinnedMeshRenderer> list, SkinnedMeshRenderer candidate)
+    {
+        return true;
+    }
+    
+    public override void OnInspectorGUI()
+    {
+        t = (d4rkAvatarOptimizer)target;
+
+        t.MergeBackFaceCullingWithCullingOff =
+            EditorGUILayout.Toggle("Merge Cull Back with Cull Off", t.MergeBackFaceCullingWithCullingOff);
+        
+        if (GUILayout.Button("Create Optimized Copy"))
+        {
+            var copy = GameObject.Instantiate(t.gameObject);
+            DestroyImmediate(copy.GetComponent<d4rkAvatarOptimizer>());
+            var boneCapsule = copy.GetComponentInChildren<BoneCapsule>(true);
+            if (boneCapsule != null)
+                DestroyImmediate(boneCapsule);
+            copy.name = t.gameObject.name + "(OptimizedCopy)";
+            copy.SetActive(true);
+            t.gameObject.SetActive(false);
+            Selection.objects = new Object[] { copy };
+        }
+
+        var skinnedMeshRenderers = t.GetComponentsInChildren<SkinnedMeshRenderer>(true);
+
+        var matchedSkinnedMeshes = new List<List<SkinnedMeshRenderer>>();
+        foreach (var skinnedMeshRenderer in skinnedMeshRenderers)
+        {
+            if (skinnedMeshRenderer == null)
+                continue;
+
+            var mesh = skinnedMeshRenderer.sharedMesh;
+            if (mesh == null)
+                continue;
+
+            bool foundMatch = false;
+            foreach (var subList in matchedSkinnedMeshes)
+            {
+                if(CanCombineWith(subList, skinnedMeshRenderer))
+                {
+                    subList.Add(skinnedMeshRenderer);
+                    foundMatch = true;
+                    break;
+                }
+            }
+            if (!foundMatch)
+            {
+                matchedSkinnedMeshes.Add(new List<SkinnedMeshRenderer> { skinnedMeshRenderer });
+            }
+        }
+
+        foreach (var mergedMeshes in matchedSkinnedMeshes)
+        {
+            EditorGUILayout.Separator();
+            EditorGUILayout.LabelField(mergedMeshes[0].name);
+            foreach (var mesh in mergedMeshes)
+            {
+                foreach (var material in mesh.sharedMaterials)
+                {
+                    EditorGUILayout.LabelField("  " + mesh.name + "." + material.name);
+                }
+            }
+        }
+    }
+}
+#endif
