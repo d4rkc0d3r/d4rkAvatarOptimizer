e192de6dfbf0fa611a6c568c7578b8f42f1bdea0
d4rkc0d3r <1735522+d4rkc0d3r@users.noreply.github.com>
2021-06-15 18:51:36 +0200

    started shader analyzer stuff

---
+++ ShaderAnalyzer.cs
@@ -0,0 +1,140 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Globalization;
+using System.Linq;
+
+namespace d4rkpl4y3r
+{
+    public class ShaderAnalyzer
+    {
+        public Shader shader;
+        public readonly List<string> rawLines = new List<string>();
+        public readonly List<string> processedLines = new List<string>();
+        public readonly List<string> properties = new List<string>();
+
+        public ShaderAnalyzer()
+        {
+
+        }
+
+        public ShaderAnalyzer(Shader shader)
+        {
+            this.shader = shader;
+        }
+
+        public void Parse()
+        {
+            rawLines.Clear();
+            processedLines.Clear();
+            properties.Clear();
+            ReadRawLines();
+            ProcessRawLines();
+            ParsePropertyBlock();
+        }
+
+        private void ReadRawLines()
+        {
+            string filePath = AssetDatabase.GetAssetPath(shader);
+            string fileContents = null;
+            try
+            {
+                StreamReader sr = new StreamReader(filePath);
+                fileContents = sr.ReadToEnd();
+                sr.Close();
+            }
+            catch (FileNotFoundException e)
+            {
+                Debug.LogError("Shader file " + filePath + " not found.  " + e.ToString());
+                return;
+            }
+            catch (IOException e)
+            {
+                Debug.LogError("Error reading shader file.  " + e.ToString());
+                return;
+            }
+            rawLines.AddRange(Regex.Split(fileContents, "\r\n|\r|\n"));
+        }
+
+        private void ProcessRawLines()
+        {
+            for (int i = 0; i < rawLines.Count; i++)
+            {
+                string trimmedLine = rawLines[i].Trim();
+                if (trimmedLine == "")
+                    continue;
+                if (trimmedLine.EndsWith("{"))
+                {
+                    trimmedLine = trimmedLine.Substring(0, trimmedLine.Length - 1).TrimEnd();
+                    if (trimmedLine != "")
+                        processedLines.Add(trimmedLine);
+                    processedLines.Add("{");
+                    continue;
+                }
+                processedLines.Add(trimmedLine);
+            }
+        }
+
+        private void ParsePropertyBlock()
+        {
+            bool isInPropertyBlock = false;
+            int propertyBlockCurlyBracketDepth = -1;
+            int curlyBracketDepth = 0;
+            for (int i = 0; i < processedLines.Count; i++)
+            {
+                string line = processedLines[i];
+                if (line == "{")
+                {
+                    curlyBracketDepth++;
+                }
+                else if (line == "}")
+                {
+                    curlyBracketDepth--;
+                    if (isInPropertyBlock && curlyBracketDepth == propertyBlockCurlyBracketDepth)
+                    {
+                        isInPropertyBlock = false;
+                        return;
+                    }
+                }
+                else if (line == "Properties" && processedLines[i + 1] == "{")
+                {
+                    isInPropertyBlock = true;
+                    propertyBlockCurlyBracketDepth = curlyBracketDepth;
+                    curlyBracketDepth++;
+                    i++;
+                }
+                else if (isInPropertyBlock)
+                {
+                    string modifiedLine = line;
+                    int squareBracketOpenIndex = line.IndexOf('[');
+                    while (squareBracketOpenIndex != -1)
+                    {
+                        int closeBracketIndex = modifiedLine.IndexOf(']') + 1;
+                        if (closeBracketIndex != 0)
+                        {
+                            modifiedLine = modifiedLine.Substring(0, squareBracketOpenIndex)
+                                + modifiedLine.Substring(closeBracketIndex, modifiedLine.Length - closeBracketIndex);
+                            squareBracketOpenIndex = modifiedLine.IndexOf('[');
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    modifiedLine = modifiedLine.Trim();
+                    int parentheses = modifiedLine.IndexOf('(');
+                    if (parentheses != -1)
+                    {
+                        properties.Add(modifiedLine);
+                    }
+                }
+            }
+        }
+    }
+}
+#endif
---
+++ ShaderAnalyzerDebugger.cs
@@ -0,0 +1,53 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using d4rkpl4y3r;
+
+public class ShaderAnalyzerDebugger : EditorWindow
+{
+    private Material mat;
+    private ShaderAnalyzer analyzer = new ShaderAnalyzer();
+    private int maxLines = 5;
+    private int maxProperties = 20;
+
+    [MenuItem("Window/Shader Analyzer Debugger")]
+    static void Init()
+    {
+        GetWindow(typeof(ShaderAnalyzerDebugger));
+    }
+
+    public void OnGUI()
+    {
+        mat = EditorGUILayout.ObjectField("Material", mat, typeof(Material), false) as Material;
+        maxLines = EditorGUILayout.IntField("Max Lines", maxLines);
+        maxProperties = EditorGUILayout.IntField("Max Properties", maxProperties);
+        analyzer.shader = mat?.shader;
+
+        if (GUILayout.Button("Analyze"))
+        {
+            analyzer.Parse();
+        }
+
+        for (int i = 0; i < maxLines && i < analyzer.rawLines.Count; i++)
+        {
+            GUILayout.Label(analyzer.rawLines[i]);
+        }
+
+        GUILayout.Space(20);
+
+        for (int i = 0; i < maxLines && i < analyzer.processedLines.Count; i++)
+        {
+            GUILayout.Label(analyzer.processedLines[i]);
+        }
+
+        GUILayout.Space(20);
+
+        for (int i = 0; i < maxProperties && i < analyzer.properties.Count; i++)
+        {
+            GUILayout.Label(analyzer.properties[i]);
+        }
+    }
+}
+#endif
