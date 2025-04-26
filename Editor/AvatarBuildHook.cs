290cf55682e3f8e4261c2ff89d8d17620af631e7
ZenithVal <88603991+ZenithVal@users.noreply.github.com>
2025-04-26 15:59:23 -0400

    Toggle for optimizing in play mode (#149)

--- Editor/AvatarBuildHook.cs
+++ Editor/AvatarBuildHook.cs
@@ -41,10 +41,17 @@ namespace d4rkpl4y3r.AvatarOptimizer
             }
             try
             {
-                if (Application.isPlaying && didRunInPlayMode)
+                if (Application.isPlaying)
                 {
-                    Debug.LogWarning($"Only one avatar can be optimized per play mode session. Skipping optimization of {avatarGameObject.name}");
-                    return true;
+                    if (!AvatarOptimizerSettings.DoOptimizeInPlayMode)
+                    {
+                        return true;
+                    }
+                    else if (didRunInPlayMode)
+                    {
+                        Debug.LogWarning($"Only one avatar can be optimized per play mode session. Skipping optimization of {avatarGameObject.name}");
+                        return true;
+                    }
                 }
                 didRunInPlayMode = Application.isPlaying;
                 optimizer.Optimize();
